998b6022d8ee171a79bddb19d034069cb24af943
Michal Szorád <miso.szorad@gmail.com>
2018-06-10 14:49:33 +0200

    Add tracking manager

---
+++ src/tracking/__tests__/manager.js
@@ -0,0 +1,46 @@
+const { getCount, increaseCount, saveData } = require("../manager");
+
+const createMocks = () => {
+  return {
+    append: jest.fn(),
+    get: jest.fn(),
+    put: jest.fn(),
+  };
+};
+
+describe("getCount", () => {
+  it("Calls get function and returns it's number value", async () => {
+    const { get } = createMocks();
+    const returnValue = "123";
+    const result = await getCount({ get: get.mockReturnValue(returnValue) });
+    expect(result).toBe(+returnValue);
+  });
+
+  it("Calls get function and returns 0 if given value is invalid", async () => {
+    const { get } = createMocks();
+    const result = await getCount({ get: get.mockReturnValue(null) });
+    expect(result).toBe(0);
+  });
+});
+
+describe("increaseCount", () => {
+  it("Calls get function and returns it's number value", async () => {
+    const { get, put } = createMocks();
+    const a = 10;
+    const b = 5;
+    await increaseCount(a, { get: get.mockReturnValue(b), put });
+    expect(put.mock.calls.pop()).toContain(a + b);
+  });
+});
+
+describe("saveData", () => {
+  it("Stringifies data and saves it to logs.txt file", async () => {
+    const { append } = createMocks();
+    const data = { a: "foo", b: [5, "bar", null] };
+    await saveData(data, { append });
+    expect(append.mock.calls.pop()).toEqual([
+      "logs.txt",
+      JSON.stringify(data),
+    ]);
+  });
+});
---
+++ src/tracking/manager.js
@@ -0,0 +1,21 @@
+const { store, load } = require("../db/adapter");
+const { appendFile } = require("../fs/adapter");
+
+async function increaseCount(amount, { get = getCount, put = store } = {}) {
+  const count = await get();
+  await put("count", count + amount);
+}
+
+async function getCount({ get = load } = {}) {
+  return parseFloat(await get("count")) || 0;
+}
+
+async function saveData(dataObject, { append = appendFile } = {}) {
+  return await append("logs.txt", JSON.stringify(dataObject));
+}
+
+module.exports = {
+  getCount,
+  increaseCount,
+  saveData,
+};
